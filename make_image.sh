f0274db1d3fec702a6d4b61c8c2a4e94c11beff5
Andre Zeps <andre.zeps@googlemail.com>
2025-03-03 21:25:22 +0100

    More reproducible build

--- make_image.sh
+++ make_image.sh
@@ -15,28 +15,32 @@ time WINEPATH=D:/DOS/BIN wine D:/dos/bin/bmake.exe link
 time SDL_VIDEODRIVER=dummy dosbox -conf dosbox.conf master.bat -exit
 
 # The error codes are not available on MSDOS. We check for the log to be sure
+cat build/MASTER.LOG
 grep "End   generation of album" build/MASTER.LOG
 
 # Convert the CDI/TOC files into CUE/BIN using mouse automation
 
 wine tocsplit.exe&
+sleep 0.3
 WID=$(xdotool search --name --sync tocsplit)
 xdotool windowactivate --sync $WID
 # Click "Open TOC"
 xdotool mousemove --sync -w $WID 220 340
 xdotool click -w $WID 1
 # Double-Click on first image
-sleep 0.2
+sleep 0.3
 xdotool mousemove --sync -w $WID 40 40
 xdotool click --repeat 2 --delay 50 1
 # Click on GO!
 xdotool mousemove --sync -w $WID 320 340
 xdotool click -w $WID 1
-sleep 0.2
+sleep 0.3
 # Click on Exit
 xdotool mousemove --sync -w $WID 500 340
 xdotool click -w $WID 1
 
 ls -lh disk/*.CUE disk/*.BIN
+
+sed -i -e "s/FILE .*\\\\/FILE /" disk/*.CUE
 
 echo " --- Done! ---"
