f341168022e3e0849dc843a4c6c1a6e8d973b6cb
Andre Zeps <andre.zeps@googlemail.com>
2026-07-13 13:21:35 +0200

    Burn cycle at the Bar

---
+++ stub_load.sh
@@ -0,0 +1,19 @@
+set -e
+
+# Compile
+WINEPATH=D:/DOS/BIN wine D:/dos/bin/bmake.exe link_app
+
+# Stop previous execution if already running by sending Ctrl+C
+stty -F /dev/ttyUSB0 9600 || stty -F /dev/ttyUSB0 9600
+
+echo -ne '\x03' > /dev/ttyUSB0
+# The CD-i will now reset
+
+# And go. Hopefully in time!
+wine cdilink.exe -port 5 -n -a 8000 -d build/nobelia.app -e
+
+# Have a terminal
+rm -f log_vmpeg
+minicom -D /dev/ttyUSB0 -b 9600 -C log_vmpeg
+
+# venv/bin/python tovcd.py
